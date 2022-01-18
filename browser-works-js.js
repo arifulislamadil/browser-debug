69057e8c3dcf282aa42f981731c9f05243538d21
Arifu Islam Adil <adil.islam.arif@gmail.com>
2022-01-18 20:19:01 +0600

    browser-debug

---
+++ browser-works-js.js
@@ -0,0 +1,9 @@
+function doSomething() {
+    console.log("I'm coding javascript");
+}
+
+console.log("I'm first person");
+console.log("I'm second person");
+setTimeout(doSomething, 500)
+console.log("I'm third person");
+console.log("I'm forth person");
