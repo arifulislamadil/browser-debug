8d6758164a5d9ad795809c1126e660923fb93e3c
Arifu Islam Adil <adil.islam.arif@gmail.com>
2022-01-19 22:19:44 +0600

    setTimeout in javascript

--- browser-works-js.js
+++ browser-works-js.js
@@ -4,6 +4,12 @@ function doSomething() {
 
 console.log("I'm first person");
 console.log("I'm second person");
-setTimeout(doSomething, 500)
+// setTimeout(doSomething, 500);
+setTimeout(function () {
+    console.log("I'm using VS code");
+}, 4000);
+setTimeout(() => {
+    console.log("Exploring MDN Articales");
+}, 2000)
 console.log("I'm third person");
 console.log("I'm forth person");
